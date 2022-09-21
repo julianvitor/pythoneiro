8daa7dd81b1c500a75d56185f6d91ff48e71d9fe
Julian <julian.vtr@gmail.com>
2022-09-21 03:54:58 -0300

    tipos

---
+++ teste.py
@@ -0,0 +1,2 @@
+x = bool(0)
+print(x)
---
+++ tipos.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+from ctypes.wintypes import HACCEL
+from re import A, I, L
+
+
+variavel = 5
+print (type(X))
+
+#tipos 
+A = str("string")
+B = int(1)
+C = float(20.5)
+D = complex(1j)
+E = list(("isso","é","uma","lista"))
+F = tuple(("isso","é","uma","tupla"))
+G = dict(isso="é dicionario", quantidade=1)
+H = set(("isso","é","um","set"))
+I = bool()
+J
+K
+L
