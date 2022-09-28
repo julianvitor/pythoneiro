56d42a09bdd300ac49bc5e510bb02bbb177a91e4
Julian <julian.vtr@gmail.com>
2022-09-28 18:05:22 -0300

    numeros

---
+++ numeros.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+#numeros
+x,y,y2,z = 1, 1.0, 1E10, 1j
+print (type(x),type(y),type(y),type(z))
+
+#conversão 
+x = float(x)
+y = str(y)
+y2 = complex(y2)
+z = str(z) #complexos não podem ser convertidos para outros formatos de numeros
+print(type(x), type(y), type(y2), type(z))
+
